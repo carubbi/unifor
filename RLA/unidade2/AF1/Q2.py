c9bc5c692b4a318df4c3cb317f203a2c66af8611
Ricardo Carubbi <carubbi@unifor.br>
2024-05-10 14:44:30 -0300

    Create Q2.py

---
+++ RLA/unidade2/AF1/Q2.py
@@ -0,0 +1,14 @@
+# Questão 2 (1 ponto)
+# Implemente em Python, um algoritmo para calcular o novo salário de um funcionário. 
+# Sabe-se que os funcionários que recebem atualmente salário de até R$ 500 terão aumento de 20%; 
+# os demais terão aumento de 10%.
+
+def reajuste_salario():
+    sal_atual = float(input("Digite seu salário atual: "))
+
+    if sal_atual <= 500:
+        sal_reaj = sal_atual * 1.2
+    else:
+        sal_reaj = sal_atual * 1.1
+
+    print(f"O novo salário é R$ {sal_reaj:.2f}")
