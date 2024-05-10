d0ec35fbffeafe671352297c05470ff7b9255a35
Ricardo Carubbi <carubbi@unifor.br>
2024-05-10 14:47:39 -0300

    Create Q4.py

---
+++ RLA/unidade2/AF1/Q4.py
@@ -0,0 +1,16 @@
+# Questão 4 (1 ponto)
+# Implemente em Python, um algoritmo que, a partir da idade do candidato(a), 
+# determinar se pode ou não tirar a CNH. Caso não atender a restrição de idade, 
+# calcular quantos anos faltam para o candidato estar apto.
+
+def verificar_apto_cnh():
+    idade = int(input("Digite a sua idade: "))
+
+    if idade < 0:
+        print("A idade deve ser maior que zero!")
+    else:
+        if idade >= 18:
+            print("O candidato está apto a tirar a CNH!")
+        else:
+            anos_apto = 18 - idade
+            print(f"Faltam {anos_apto} ano(s) para o candidato estar apto!")
