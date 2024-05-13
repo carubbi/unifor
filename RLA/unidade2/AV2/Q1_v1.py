f939397c6ac8b788c0d4d1b9cfaed2ba67b1fe8d
Ricardo Carubbi <carubbi@unifor.br>
2024-05-13 12:21:10 -0300

    Update Q1_v1.py

--- RLA/unidade2/AV2/Q1_v1.py
+++ RLA/unidade2/AV2/Q1_v1.py
@@ -1,6 +1,8 @@
 """
 Questão 01
-Desenvolva um programa para um jogo de adivinhação. O programa deve gerar aleatoriamente um número inteiro entre um valor mínimo e máximo definidos pelo usuário. O jogador terá um número limitado de tentativas para adivinhar o número gerado pelo programa. Após cada tentativa do jogador, o programa deve informar se o número número fornecido pelo jogador é maior ou menor ao número aleatório. O jogo continuará até que o jogador adivinhe corretamente o número aleatório ou esgote todas as tentativas.
+Desenvolva um programa para um jogo de adivinhação. O programa deve gerar aleatoriamente um número inteiro entre um valor mínimo e máximo definidos pelo usuário. 
+O jogador terá um número limitado de tentativas para adivinhar o número gerado pelo programa. Após cada tentativa do jogador, o programa deve informar se o número 
+número fornecido pelo jogador é maior ou menor ao número aleatório. O jogo continuará até que o jogador adivinhe corretamente o número aleatório ou esgote todas as tentativas.
 """
 from random import randint
 
